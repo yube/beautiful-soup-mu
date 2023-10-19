8cba5d8693b123f56e2b0fd644235ccdb7962553
yube <ale97@live.it>
2023-10-19 15:39:45 +0200

    Update main-with-login.py
    updated to replace deprecated functions
    added a longer timer because mu was getting mad
    print the series getting added to screen for easier access

--- main-with-login.py
+++ main-with-login.py
@@ -3,18 +3,24 @@ import requests
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 from urllib.parse import urljoin
+import time
 
-# You can edit these
-# BEGIN_PAG and END_PAG to change where program begins and ends (https://www.mangaupdates.com/releases.html)
-# username and password are needed to show eromanga related covers because MU doesn't display them if you're not logged in, if you don't want to login use the other .py file
 BEGIN_PAG = 1
-END_PAG = 15
+END_PAG = 10
+
 username = 'usr'
 password = 'pwd'
 
+# Initialize session object to manage cookies
 session = requests.Session()
+
+# Login URL
 login_url = 'https://www.mangaupdates.com/login.html'
+
+# Prepare the payload with your username and password
 payload = {'username': username, 'password': password, 'act': 'login'}
+
+# Perform login
 response = session.post(login_url, data=payload)
 
 def resize_image(img, max_height=224):
@@ -23,7 +29,7 @@ def resize_image(img, max_height=224):
         new_height = max_height
         aspect_ratio = width / height
         new_width = int(new_height * aspect_ratio)
-        img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
     return img
 
 
@@ -34,6 +40,7 @@ def parse_series_page(url):
     for type_tag in series_soup.find_all('div', {'class': 'sContent'}):
         type_text = type_tag.get_text().strip()
         if type_text == "Manhwa" or type_text == "Manhua" or type_text == "Doujinshi":
+            # print (url, 'hwa, hua, shi')
             return None  # Return None to indicate this should be skipped
 
     # Check for "Completely Scanlated? No"
@@ -42,8 +49,8 @@ def parse_series_page(url):
         cat_text = cat_div.get_text().strip()
         content_text = content_div.get_text().strip()
         if "Completely Scanlated?" in cat_text and content_text == "No":
+            # print(url, 'not complete')
             return None  # Return None to indicate this should be skipped
-
 
     img_tags = series_soup.find_all('img', {'class': 'img-fluid'})
     if len(img_tags) >= 4:
@@ -58,8 +65,10 @@ def parse_series_page(url):
             img = Image.open(BytesIO(img_response.content))
             return img
         except requests.exceptions.MissingSchema:
-            print("Invalid URL for image:", img_url)
+            # print("Invalid URL for image:", img_url)
             return None
+    else:
+        # print(url, img_tags)
     return None
 
 
@@ -115,8 +124,8 @@ def create_montage(images, titles, first_date, last_date, images_per_row=10):
         title_font = ImageFont.load_default()
 
     # Draw title
-    title_text = f"Series completed from {first_date} to {last_date}"
-    title_width, title_height_actual = draw.textsize(title_text, font=title_font)
+    title_text = f"Series completed from {last_date} to {first_date}"
+    title_width = draw.textlength(title_text, font=title_font)
     title_position = ((montage_width - title_width) // 2, 10)  # X-center the text
     draw.text(title_position, title_text, font=title_font, fill=(0, 0, 0))
 
@@ -143,8 +152,9 @@ ended_series = []
 date_elements = []
 
 for page_num in range(BEGIN_PAG, END_PAG+1):
+    time.sleep(10)
     url = f"https://www.mangaupdates.com/releases.html?page={page_num}"
-    print("url= ", url)
+    # print("url= ", url)
     response = session.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -159,9 +169,11 @@ for page_num in range(BEGIN_PAG, END_PAG+1):
 
             if prev_div is not None:
                 link_tag = prev_div.find('a')
-                if link_tag is not None:
+                if link_tag is not None and link_tag.get_text() != 'Add':
                     series_name = link_tag.get_text()
                     series_link = link_tag['href']
+
+                    # print(link_tag)
 
                     series_image = parse_series_page(series_link)
 
@@ -170,6 +182,7 @@ for page_num in range(BEGIN_PAG, END_PAG+1):
                     # print("series_image ", series_image)
 
                     if series_image is not None:
+                        print(f"{series_name}: {series_link}")
                         ended_series.append({
                             'name': series_name,
                             'link': series_link,
